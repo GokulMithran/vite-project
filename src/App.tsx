4138d10bd0dc5c250061132df10105677d83dc69
GokulMithran <gokulnth781@gmail.com>
2025-08-19 08:41:29 +0530

    Passind Data via Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import ListGroup from "./components/ListGroup";
 
-function App(){
-  return <div>
-    <ListGroup />
-  </div>
+let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+let heading = "Cities";
+
+function App() {
+  return (
+    <div>
+      <ListGroup items={items} heading={heading} />
+    </div>
+  );
 }
 
 export default App;
